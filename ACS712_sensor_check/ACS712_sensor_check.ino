842587852385d8495a07cc9c3af9f33bbd1428f3
Yasee Rathnayaka <rmybrathnayaka@std.appsc.sab.ac.lk>
2024-01-23 23:27:05 +0530

    ACS712 Sensor check

--- ACS712_sensor_check/ACS712_sensor_check.ino
+++ ACS712_sensor_check/ACS712_sensor_check.ino
@@ -7,10 +7,10 @@ void setup() {
 void loop() {
   int sensorValue = analogRead(ACS712_PIN);
   
-  // Assuming 5V reference voltage for Arduino
+ 
   float voltage = sensorValue * (5.0 / 1023.0);
 
-  // ACS712 sensitivity is 100mV/A for ACS712-05B, adjust if you have a different model
+ 
   float current = (voltage - 2.5) / 0.1;
 
   Serial.print("Sensor Value: ");

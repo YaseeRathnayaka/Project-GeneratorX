511a30cc4a1941bd44eed578e2dc4db2da8fd4e7
Yasee Rathnayaka <rmybrathnayaka@std.appsc.sab.ac.lk>
2024-01-23 23:22:47 +0530

    Update DHT22 code

--- DHT22_sensor_check/DHT22_sensor_check.ino
+++ DHT22_sensor_check/DHT22_sensor_check.ino
@@ -3,10 +3,9 @@
 // Pin to which the DHT22 sensor data pin is connected
 #define DHT_PIN 2
 
-// Uncomment one of the following lines based on your DHT sensor model
-//#define DHT_TYPE DHT11   // For DHT11
+
 #define DHT_TYPE DHT22   // For DHT22
-//#define DHT_TYPE DHT21   // For DHT21
+
 
 DHT dht(DHT_PIN, DHT_TYPE);
 
